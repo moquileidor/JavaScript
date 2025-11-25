81d2020cc9b99b1bf88ea185666815cd11110401
Jorge Alvarado <jalvaradocuesta@gmail.com>
2025-11-24 19:15:24 -0500

    aplicando el ejercicio  de la clase 2

--- Ejercicio.js
+++ Ejercicio.js
@@ -1,1 +1,15 @@
-console.log("Hola Mundo");
+let promedio = (70 + 82 + 50 + 20) / 4;
+console.log("El promedio es: " + promedio);
+
+let = num1 = 6;
+let resultado = num1 ** 3;
+console.log(resultado);
+
+let nombre = "Jorge";
+let edad;
+edad = 22;
+
+let residencia = "Medellin, Colombia"
+
+console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + residencia + ".");
+
